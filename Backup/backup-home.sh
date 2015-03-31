6c7a4ab70841f8e3bd740c946718dcf9c7843d31
Sujay Phadke <electronicsguy123@gmail.com>
2015-03-31 17:30:41 -0400

    Minor improvements Copyright notice

--- Backup/backup-home.sh
+++ Backup/backup-home.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Copyright (C) Sujay Phadke, 2015
 
 SOURCE="/home/pi/"
 DEST="/media/root/home/pi/"
@@ -11,32 +12,46 @@ LOGFILE="/home/pi/Utils/Backup/rsync.log"
 sudo mountpoint -q $DESTMOUNTDIR
 MOUNTED=$?
 
+# Check if partition is mounted. "0" means Yes.
 if (( $MOUNTED == 0 ));
 then
   echo -e "$DESTMOUNTDEV already mounted. Continuing with backup ...\n"
 else
-  sudo mount $DESTMOUNTDEV $DESTMOUNTDIR
-  if (( $? != 0 )) ;
+
+  if [[ (-n $1) && ($1 == "auto") ]];
+  then
+    # Automount partition
+    sudo mount $DESTMOUNTDEV $DESTMOUNTDIR
+    if (( $? != 0 )) ;
     then
-    echo -e "Error! : Cannot mount $DESTMOUNTDEV for backup. Exiting.\n"
+      echo -e "Error! : Cannot mount $DESTMOUNTDEV for backup. Exiting.\n"
+      exit 1
+    fi
+    
+  else
+    echo -e "Partition '$DESTMOUNTDEV' is not mounted at '$DESTMOUNTDIR'!"
+    echo -e "Use automount if necessary.\n"
     exit 1
   fi
+
 fi
 
-if ! [[ -d $DEST ]];
+if ! [[ -w $DEST ]];
 then
-    echo -e "The destination directory '$DEST' doesn't exist or is not mounted."
-    echo -e "If this is the first backup, uncomment the 'exit' below.\n"
+    echo -e "The destination directory '$DEST' is not writable. Verify permissions."
+    echo -e "If this is the first backup, uncomment the 'exit' statement below.\n"
     exit 1
 fi 
 
-sudo rsync -avuh  --delete --stats --log-file=$LOGFILE --exclude-from $EXCLUDE $SOURCE $DEST
+# Perform differential sync
+sudo rsync -avuh --delete --stats --log-file=$LOGFILE --exclude-from $EXCLUDE $SOURCE $DEST
 
 if (( $MOUNTED == 0 ));
 then
-  echo -e "\nFinished. Leaving $DESTMOUNTDEV mounted.\n"
+  echo -e "\nFinished backup. Leaving $DESTMOUNTDEV mounted.\n"
   exit 0
 else
+  # auto unmount
   sudo umount $DESTMOUNTDIR
   if (( $? != 0 )) ;
   then
