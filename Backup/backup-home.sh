06af1e4cb15c9b1f7ee1194b0e0e42857593a621
Sujay Phadke <electronicsguy123@gmail.com>
2015-03-30 20:44:41 -0400

    Added automount for backup volumes

--- Backup/backup-home.sh
+++ Backup/backup-home.sh
@@ -2,14 +2,47 @@
 
 SOURCE="/home/pi/"
 DEST="/media/root/home/pi/"
-EXCLUDE="/home/pi/Utils/backup/rsync-home.exclude"
-LOGFILE="/home/pi/Utils/backup/rsync.log"
+DESTMOUNTDEV="/dev/mmcblk0p6"
+DESTMOUNTDIR="/media/root"
+EXCLUDE="/home/pi/Utils/Backup/rsync-home.exclude"
+LOGFILE="/home/pi/Utils/Backup/rsync.log"
+
+# Check if backup device is already mounted
+sudo mountpoint -q $DESTMOUNTDIR
+MOUNTED=$?
+
+if (( $MOUNTED == 0 ));
+then
+  echo -e "$DESTMOUNTDEV already mounted. Continuing with backup ...\n"
+else
+  sudo mount $DESTMOUNTDEV $DESTMOUNTDIR
+  if (( $? != 0 )) ;
+    then
+    echo -e "Error! : Cannot mount $DESTMOUNTDEV for backup. Exiting.\n"
+    exit 1
+  fi
+fi
 
 if ! [[ -d $DEST ]];
 then
-    echo -e "The destination directory '$DEST' either doesn't exist or isn't writable!."
-    echo -e "If this is the first backup, uncomment the 'exit' below."
-    exit
+    echo -e "The destination directory '$DEST' doesn't exist or is not mounted."
+    echo -e "If this is the first backup, uncomment the 'exit' below.\n"
+    exit 1
 fi 
 
 sudo rsync -avuh  --delete --stats --log-file=$LOGFILE --exclude-from $EXCLUDE $SOURCE $DEST
+
+if (( $MOUNTED == 0 ));
+then
+  echo -e "\nFinished. Leaving $DESTMOUNTDEV mounted.\n"
+  exit 0
+else
+  sudo umount $DESTMOUNTDIR
+  if (( $? != 0 )) ;
+  then
+    echo -e "Error! : Cannot unmount $DESTMOUNTDIR.\n"
+    exit 1
+  fi
+fi
+
+exit 0
--- Backup/backup-root-dryrun.sh
+++ Backup/backup-root-dryrun.sh
@@ -2,14 +2,47 @@
 
 SOURCE="/"
 DEST="/media/root/"
-EXCLUDE="/home/pi/Utils/backup/rsync-root.exclude"
-LOGFILE="/home/pi/Utils/backup/rsync.log"
+DESTMOUNTDEV="/dev/mmcblk0p6"
+DESTMOUNTDIR="/media/root"
+EXCLUDE="/home/pi/Utils/Backup/rsync-root.exclude"
+LOGFILE="/home/pi/Utils/Backup/rsync.log"
+
+# Check if backup device is already mounted
+sudo mountpoint -q $DESTMOUNTDIR
+MOUNTED=$?
+
+if (( $MOUNTED == 0 ));
+then
+  echo -e "$DESTMOUNTDEV already mounted. Continuing with backup ...\n"
+else
+  sudo mount $DESTMOUNTDEV $DESTMOUNTDIR
+  if (( $? != 0 )) ;
+    then
+    echo -e "Error! : Cannot mount $DESTMOUNTDEV for backup. Exiting.\n"
+    exit 1
+  fi
+fi
 
 if ! [[ -d $DEST ]];
 then
-    echo -e "The destination directory '$DEST' either doesn't exist or isn't writable!."
+    echo -e "The destination directory '$DEST' doesn't exist or is not mounted."
     echo -e "If this is the first backup, uncomment the 'exit' below."
     exit
 fi 
 
 sudo rsync -avuPh  --dry-run --delete --stats --log-file=$LOGFILE --exclude-from $EXCLUDE $SOURCE $DEST
+
+if (( $MOUNTED == 0 ));
+then
+  echo -e "\nFinished. Leaving $DESTMOUNTDEV mounted.\n"
+  exit 0
+else
+  sudo umount $DESTMOUNTDIR
+  if (( $? != 0 )) ;
+  then
+    echo -e "Error! : Cannot unmount $DESTMOUNTDIR.\n"
+    exit 1
+  fi
+fi
+
+exit 0
