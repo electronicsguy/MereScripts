e067c2c57fc74bb034f0d957d0bd1b2d02d1db56
Sujay Phadke <electronicsguy123@gmail.com>
2015-04-30 14:41:25 -0400

    Code cleanup

--- Backup/backpi.sh
+++ Backup/backpi.sh
@@ -7,6 +7,22 @@
 
 # need to use 'eval' to expand ~
 # ref: http://stackoverflow.com/questions/3963716/how-to-manually-expand-a-special-variable-ex-tilde-in-bash
+
+# Check for required packages
+#P1=$(dpkg-query -l |grep -w rsync | wc -l)
+#P2=$(dpkg-query -l |grep -w mount | wc -l)
+#P3=$(dpkg-query -l |grep libblkid | wc -l)
+#P4=$(dpkg-query -l |grep "util-linux" | wc -l)
+
+#PTOT=$(( $P1 + $P2 + $P3 + $P4 ))
+
+#if [[ $PTOT != 4 ]];
+#then
+#  echo -e "Error: Backpi needs the following packages to be installed:\n"
+#  echo -e "rsync, mount, libblkid, lsblk (util-linux).\n"
+#  exit 1
+#fi
+
 HOMEDIR="/home/pi"
 eval homedir=$HOMEDIR
 CFG_PATH=$HOMEDIR"/.backpi/"
@@ -79,28 +95,35 @@ fi
 # Start of big if-statement
 if [[ $CLI == 0 ]];
 then
-  # Retrieve relevant partition names.
-  # Keep only slave partition names
-  #mapfile BACKUPPARTS -t < <(sudo lsblk -l -o name,label | grep -i backup | sed 's/ $//' )
-  
-  # get / and /boot device names
+  # Check if whiptail is installed
+  #WHIP=$(dpkg-query -l |grep whiptail | wc -l)
+  #if [[ $WHIP == 0 ]];
+  #then
+  #  echo -e "Error: the package 'whiptail' is not installed and is required for interactive mode.\n"
+  #  echo -e "Either install the package or run Backpi in command-line only mode.\n"
+  #  exit 1
+  #fi
+
+  # get root FS '/' and '/boot' device names
   EXCL_ARRAY=(`sudo lsblk  -l -n -o name,type,mountpoint |grep -v disk | grep -E "/$|/boot$" | awk '{print $1}'`)
-  
+
   # Join array elements ref: http://stackoverflow.com/questions/1527049/bash-join-elements-of-an-array
   IFS="|" eval 'EXCL_LIST="${EXCL_ARRAY[*]}"'
-  
+
   # add exclusions from config file
   if ! [[ -z $DEFEXCL ]];
   then
     DEFEXCL=$(echo $DEFEXCL | sed -e 's/ /|/g')
     EXCL_LIST=$EXCL_LIST"|"$DEFEXCL
   fi
-  
+
+  # Retrieve relevant partition names
+  # Keep only slave partition names
   # Exclude non-eligible partitions
   mapfile BACKUPPARTS -t < <(sudo lsblk -l -n -o name,label,type | grep -v disk | perl -pe 's/part//g' | grep -Ev $EXCL_LIST)
-  
+
   NUMPARTS=${#BACKUPPARTS[@]}
-  
+
   if [[ $NUMPARTS == 0 ]];
   then
     echo -e "No valid partitions found. Specify one on the command line.\n"
@@ -108,37 +131,37 @@ then
     sudo lsblk -l -n -o name,type | grep -v disk | awk '{print $1}'
     exit 1
   fi
-  
+
   PARTLIST=''
   for (( i=0; i<$NUMPARTS; i++ ));
   do
     PARTLIST=$PARTLIST" "$i" "\"${BACKUPPARTS[$i]}\"
   done
-  
+
   # Remove extra spaces
   PARTLIST=$(echo $PARTLIST | sed 's/ " /" /g')
   PARTLIST=$(echo $PARTLIST | sed 's/ "$/"/')
-  
+
   if [[ $NUMPARTS > 10 ]];
   then
     NUMROWS=10
   else
     NUMROWS=$NUMPARTS
   fi
-  
-  # Bash parameter array for running a command: 
+
+  # Bash parameter array for running a command
   # http://stackoverflow.com/questions/11079342/execute-command-containing-quotes-from-shell-variable
   COMMAND="whiptail --title \"Mera Backup\" --menu \"Select the destination backup partition (/, /boot, 'SETTINGS' and 'RECOVERY' partitions have been excluded)\" --cancel-button Cancel --ok-button Select --notags   20 50 $NUMROWS $PARTLIST 3>&1 1>&2 2>&3"
-  
+
   OPTION=$(eval "$COMMAND")
   BUTTON=$?
-  
-  # Check if 'Cancel' or ESC pressed
+
+  # Check if 'Cancel' or 'ESC' pressed
   if [[ ($BUTTON == 1) || ($BUTTON = 255) ]];
   then
     exit 1
   fi
-  
+
   # get partial name of device
   PARTDEV=$(echo ${BACKUPPARTS[$OPTION]} | cut -d " " -f 1)
   DESTMOUNTDEV="/dev/"$PARTDEV
@@ -154,14 +177,14 @@ then
     # Bad name
     DESTMOUNTDIR=''
   fi
-  
+
   if [[ $DESTMOUNTDIR == "" ]];
   then
     echo -e "Destination device $DESTMOUNTDEV either doesn't have a label or "
     echo -e "the label contains special characters (mounting problems).\n"
     # Check if backup device is already mounted
     MOUNTED=$(sudo mount | grep -i $DESTMOUNTDEV)
-  
+
     if [[ $MOUNTED != "" ]];
     then
       echo -e "Device $DESTMOUNTDEV is already mounted. We need to unmount it first and"
@@ -180,24 +203,24 @@ then
         exit 1
       fi
     fi
-  
+
     # Get mount directory from user
     while true
     do
       echo -e "Please enter destination mount directory without"
       echo -e "any special characters or spaces (default: /media/root): "
       read DESTMOUNTDIR
-    
+
       if [[ $DESTMOUNTDIR == "q" ]];
       then
         exit 1
       fi
-      
+
       if [[ $DESTMOUNTDIR == "" ]];
       then
         DESTMOUNTDIR="/media/root"
       fi
-      
+
       TEMP=$(echo $DESTMOUNTDIR | grep -i '^\/media\/')
       if [[ $TEMP == "" ]];
       then
@@ -206,18 +229,18 @@ then
         break
       fi
     done
-    
+
   fi
-  
+
   OPTION=$(whiptail --title "Mera Backup" --menu "Select the backup source" --cancel-button Cancel --ok-button Select --notags   10 50 2 0 "/home/pi/ (user: pi)" 1 "/ (entire root FS)" 3>&1 1>&2 2>&3)
   BUTTON=$?
-  
+
   # Check if 'Cancel' or ESC pressed
   if [[ ($BUTTON == 1) || ($BUTTON = 255) ]];
   then
     exit
   fi
-  
+
   case $OPTION in
   0)
     SOURCE="/home/pi/"  # This termination / is essential for rsync
@@ -227,16 +250,16 @@ then
     SOURCE="/"
     DEST=$DESTMOUNTDIR"/"  # This termination / is essential for rsync
   esac
-  
+
   ACTUAL=$(whiptail --title "Mera Backup" --menu "Select mode" --cancel-button Cancel --ok-button Select --notags   10 50 2 0 "dry-run" 1 "Actual" 3>&1 1>&2 2>&3)
   BUTTON=$?
-  
+
   # Check if 'Cancel' or ESC pressed
   if [[ ($BUTTON == 1) || ($BUTTON = 255) ]];
   then
     exit
   fi
-  
+
 fi  # endif for if $CLI == 0
 
 #=========================================================
@@ -295,7 +318,7 @@ then
       exit 1
     fi
   fi
-  
+
   EXCLUDE=$CFG_PATH$EXCLUDEHOME
 else
   EXCLUDE=$CFG_PATH$EXCLUDEROOT
